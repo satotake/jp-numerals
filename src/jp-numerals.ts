8d4e22f275f1a585a2451e4beaa7510cb5f77218
SatowTakeshi <doublequotation@gmail.com>
2019-06-22 18:03:10 +0900

    prototyping

--- src/jp-numerals.ts
+++ src/jp-numerals.ts
@@ -1,6 +1,78 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { JpNumeralUnit, NumeralObj, Numerals } from './type'
 
+export class Numeral {
+  raw: number
+  unit: JpNumeralUnit
+  constructor(unit: JpNumeralUnit, raw: number) {
+    if (raw < 0) {
+      throw Error('Number must be positive or 0')
+    }
+
+    this.raw = raw
+    this.unit = unit
+  }
+
+  get character(): string {
+    return JpNumeralUnit[this.unit]
+  }
+
+  get rank(): number {
+    return this.unit
+  }
+
+  get digits(): number {
+    return Math.floor(this.raw / Math.pow(10, this.rank * 4)) % Math.pow(10, 4)
+  }
+
+  toString(): string {
+    return `${this.digits}${this.character}`
+  }
+  toNumeralObj(): NumeralObj {
+    const { unit, character, rank, digits } = this
+    return { unit, character, rank, digits }
+  }
+}
+
+export class NumeralZero extends Numeral {
+  get character(): string {
+    return ''
+  }
+
+  get digits(): number {
+    const factor = 10 ** this.scale
+    const val = (this.raw / Math.pow(10, this.rank * 4)) % Math.pow(10, 4)
+    return Math.round(val * factor) / factor
+  }
+
+  // TODO handling decimal points
+  private get scale(): number {
+    const s = this.raw.toString()
+    return s.includes('.') ? s.split('.')[1].length : 0
+  }
+}
+
+export const jpNumerals = (n: number, base: JpNumeralUnit = JpNumeralUnit.零): Numerals => {
+  // TODO(handle negative)
+  if (n < 0) {
+    throw Error('number must be string')
+  }
+
+  const raw = Math.abs(n) * Math.pow(10, base * 4)
+  // in myriads
+
+  const unitLen = Object.keys(JpNumeralUnit).length / 2
+  const numberLen = Math.ceil(Math.log10(raw) / 4)
+  const len = Math.min(unitLen, numberLen)
+  const numerals = new Array(len)
+    .fill(NaN)
+    .map((_, i) => (i === 0 ? new NumeralZero(i, raw) : new Numeral(i, raw)))
+    .reverse()
+
+  return {
+    toArray: () => numerals,
+    toTuples: () => numerals.map(numeral => [numeral.digits, numeral.character]),
+    toNumeralObjs: () => numerals.map(numeral => numeral.toNumeralObj()),
+    toString: () => numerals.reduce((s, numeral) => `${s}${numeral}`, ''),
+    toNumber: () => raw
+  }
 }
---
+++ src/type.ts
@@ -0,0 +1,38 @@
+import { Numeral } from './jp-numerals'
+
+export enum JpNumeralUnit {
+  零,
+
+  万,
+  億,
+  兆,
+  京,
+  垓,
+  秭,
+  穣,
+  溝,
+  澗,
+  正,
+  載,
+  極,
+  恒河沙,
+  阿僧祇,
+  那由他,
+  不可思議,
+  無量大数
+}
+
+export interface NumeralObj {
+  unit: JpNumeralUnit
+  character: string
+  rank: number
+  digits: number
+}
+
+export interface Numerals {
+  toArray: () => Numeral[]
+  toTuples: () => [number, string][]
+  toNumeralObjs: () => NumeralObj[]
+  toString: () => string
+  toNumber: () => number
+}
